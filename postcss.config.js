8e800961d1432c32879717810381272502487431
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-17 11:34:18 -0500

    Remove themes from postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-const themes = require("postcss-custom-props-themes");
-const color = require("css-color-function");
+const color = require('css-color-function');
 
 module.exports = {
-  plugins: [require("cssnano")(), require("autoprefixer")(), require("postcss-color-function")()]
+  plugins: [
+    require('cssnano')(),
+    require('autoprefixer')(),
+    require('postcss-color-function')(),
+  ],
 };
